43cbce463a1619208a1496ae417427a1daa4f9b0
SadikRa <sadikrahman494@gmail.com>
2025-04-11 10:55:55 +0600

    updated done

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,8 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+
+    res.status(200).json({
+      success: true,
+      massage: "admin created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      massage: "something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
