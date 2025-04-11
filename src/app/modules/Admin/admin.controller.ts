e3b61bb24780c3bd0cf655bd3fae0355baa8314e
SadikRa <sadikrahman494@gmail.com>
2025-04-11 20:57:32 +0600

    updated

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await adminService.getAllFromDB();
+  const result = await adminService.getAllFromDB(req.query);
 
   res.status(200).json({
     success: true,
