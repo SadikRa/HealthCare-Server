2ac665dbb87c79321b42245dee6bbc8a35b8edf0
SadikRa <sadikrahman494@gmail.com>
2025-04-12 22:48:31 +0600

    updated'

--- src/helpers/paginationHelpers.ts
+++ src/helpers/paginationHelpers.ts
@@ -1,12 +1,23 @@
-const calculatePagination = (options: {
+type IOptions = {
   page?: number;
   limit?: number;
   sortOrder?: string;
+  sortBy?: string;
+};
+
+type IOptionsResult = {
+  page: number;
+  limit: number;
+  skip: number;
   sortBy: string;
-}) => {
+  sortOrder: string;
+};
+
+const calculatePagination = (options: IOptions): IOptionsResult => {
   const page: number = Number(options.page) || 1;
   const limit: number = Number(options.limit) || 10;
-  const skip: number = Number(page - 1) * limit;
+  const skip: number = (Number(page) - 1) * limit;
+
   const sortBy: string = options.sortBy || "createdAt";
   const sortOrder: string = options.sortOrder || "desc";
 
@@ -19,6 +30,6 @@ const calculatePagination = (options: {
   };
 };
 
-export const paginationHelpers = {
+export const paginationHelper = {
   calculatePagination,
 };
