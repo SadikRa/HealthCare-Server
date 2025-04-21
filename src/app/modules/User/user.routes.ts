3539cbf2ae21285b0bb8771fd33f663dab7ce497
SadikRa <sadikrahman494@gmail.com>
2025-04-21 23:30:14 +0600

    image upload done

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
+import upload from "../../../helpers/fileUploader";
 
 const router = express.Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin);
+router.post(
+  "/",
+  auth("ADMIN", "SUPER_ADMIN"),
+  upload.single("file"),
+  userController.createAdmin
+);
 
 export const userRoutes = router;
