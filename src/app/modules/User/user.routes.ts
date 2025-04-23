ab109a66cbee4f82a35870288e1bad5ce41f30f7
SadikRa <sadikrahman494@gmail.com>
2025-04-23 21:43:33 +0600

    image upload done

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,7 +2,6 @@ import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { fileUploader } from "../../../helpers/fileUploader";
-import validateRequest from "../../middlewares/validateRequest";
 import { userValidation } from "./user.validation";
 
 const router = express.Router();
@@ -12,10 +11,10 @@ router.post(
   auth("ADMIN", "SUPER_ADMIN"),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
-    
-  },
-  validateRequest(userValidation.createAdmin),
-  userController.createAdmin
+    userValidation.createAdmin.parse(JSON.parse(req.body.data));
+
+    userController.createAdmin;
+  }
 );
 
 export const userRoutes = router;
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -12,6 +12,28 @@ const createAdmin = async (req: any) => {
     const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
     req.body.data.admin.profilePhoto = uploadToCloudinary?.secure_url as string;
   }
+
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 12);
+
+  const userData = {
+    email: req.body.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: req.body.admin,
+    });
+
+    return createdAdminData;
+  });
+
+  return result;
 };
 
 export const userService = {
