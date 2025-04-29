24604ad532d7327cf3dea9a951ea62991aebf82e
SadikRa <sadikrahman494@gmail.com>
2025-04-29 10:16:32 +0600

    update my profile done

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -8,6 +8,18 @@ import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  userController.getAllFromDB
+);
+
+router.get(
+  "/me",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  userController.getMyProfile
+);
+
 router.post(
   "/create-admin",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
@@ -37,17 +49,21 @@ router.post(
   }
 );
 
-router.get(
-  "/",
-  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  userController.getAllFromDB
-);
-
 router.patch(
   "/:id/status",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(userValidation.updateStatus),
   userController.changeProfileStatus
+);
+
+router.patch(
+  "/update-my-profile",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    return userController.updateMyProfie(req, res, next);
+  }
 );
 
 export const userRoutes = router;
