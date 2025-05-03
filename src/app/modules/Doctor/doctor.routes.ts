a9ca3b6060f9c4fd0c334c94eace7d6c7849bdab
SadikRa <sadikrahman494@gmail.com>
2025-05-03 23:05:54 +0600

    router file updated done

--- src/app/modules/Doctor/doctor.routes.ts
+++ src/app/modules/Doctor/doctor.routes.ts
@@ -1,37 +1,36 @@
-import express from "express";
-import { DoctorController } from "./doctor.controller";
-import auth from "../../middlewares/auth";
-import { UserRole } from "@prisma/client";
-import validateRequest from "../../middlewares/validateRequest";
-import { DoctorValidation } from "./doctor.validation";
+import express from 'express'
+import { DoctorController } from './doctor.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
+import validateRequest from '../../middlewares/validateRequest';
+import { DoctorValidation } from './doctor.validation';
 
 const router = express.Router();
 
 // task 3
-router.get("/", DoctorController.getAllFromDB);
+router.get('/', DoctorController.getAllFromDB);
 
 //task 4
-router.get("/:id", DoctorController.getByIdFromDB);
+router.get('/:id', DoctorController.getByIdFromDB);
 
 router.patch(
-  "/:id",
-  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
-  validateRequest(DoctorValidation.update),
-  DoctorController.updateIntoDB
+    '/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+    validateRequest(DoctorValidation.update),
+    DoctorController.updateIntoDB
 );
 
 //task 5
 router.delete(
-  "/:id",
-  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  DoctorController.deleteFromDB
+    '/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    DoctorController.deleteFromDB
 );
 
 // task 6
 router.delete(
-  "/soft/:id",
-  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  DoctorController.softDelete
-);
+    '/soft/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    DoctorController.softDelete);
 
-export const DoctorRoutes = router;
+export const DoctorRoutes = router
